98d3c19fb56c4e4849a9430f2176e847abe68a5f
anassvoronn <UQzbwQCt7R3uMfz>
2024-03-10 22:58:14 +0300

    I made a window with the author detail

--- gui/src/app/author-detail/author-detail.component.ts
+++ gui/src/app/author-detail/author-detail.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import {AuthorService} from "../service/author.service";
 import {Author} from "../entity/author.model";
-import {MatSnackBar} from "@angular/material/snack-bar";
 import {ActivatedRoute} from "@angular/router";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-author-detail',
@@ -12,12 +10,10 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 })
 export class AuthorDetailComponent {
 
-    authorDetail!: FormGroup;
+    authorDetail!: Author;
     authorId!: string | null;
 
-    constructor(private formBuilder: FormBuilder,
-                private snackBar: MatSnackBar,
-                private authorService: AuthorService,
+    constructor(private authorService: AuthorService,
                 private route: ActivatedRoute) {
     }
 
@@ -26,25 +22,9 @@ export class AuthorDetailComponent {
             if (params.has('id')) {
                 this.authorId = params.get('id');
                 this.authorService.getById(this.authorId).subscribe(author => {
-                    this.authorDetail.setValue({
-                        name: author.name,
-                        gender: author.gender,
-                        birthDate: author.birthDate,
-                        deathDate: author.deathDate,
-                        country: author.country,
-                        age: author.age
-                    });
+                    this.authorDetail = author;
                 })
             }
-        });
-
-        this.authorDetail = this.formBuilder.group({
-                    name: ['', Validators.required],
-                    gender: ['', Validators.required],
-                    birthDate: [''],
-                    deathDate: [''],
-                    country: ['', Validators.required],
-                    age: ['']
         });
     }
 }
