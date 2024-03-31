9407893156096c12952132d336287b10ccdfd32e
anassvoronn <UQzbwQCt7R3uMfz>
2024-03-31 15:04:39 +0300

    added ts, html and other files

---
+++ gui/src/app/entity/book.model.ts
@@ -0,0 +1,38 @@
+export class Book {
+    private readonly _id: number;
+    private readonly _title: string;
+    private readonly _publishingYear: number;
+    private readonly _genre: string;
+
+    constructor(_id: number, _title: string, _publishingYear: number, _genre: string){
+        this._id = id;
+        this._title = title;
+        this._publishingYear = publishingYear;
+        this._genre = genre;
+    }
+
+    toJsonString(): string {
+            let json = JSON.stringify(this);
+            Object.keys(this).filter(key => key[0] === "_").forEach(key => {
+                json = json.replace(key, key.substring(1));
+            });
+
+            return json;
+    }
+
+     get id(): number {
+         return this._id;
+     }
+
+     get title(): string {
+         return this._title;
+     }
+
+     get publishingYear(): number {
+         return this._publishingYear;
+     }
+
+     get genre(): string {
+         return this._genre;
+     }
+}
---
+++ gui/src/app/service/book.service.ts
@@ -0,0 +1,47 @@
+import {Injectable} from '@angular/core';
+import {Book} from "../entity/book.model";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class BookService {
+    private apiUrl: string = "/book-shop/api/book";
+
+    constructor(private http: HttpClient) {
+    }
+
+    getAllBooks(): Observable<Book[]> {
+        return this.http.get<Book[]>(this.apiUrl);
+    }
+
+    getById(bookId: string | null) {
+        return this.http.get<book>(this.apiUrl + "/" + bookId);
+    }
+
+    addBook(book: Book): void {
+    }
+
+    updateBook(updatedBook: Book): Observable<string> {
+        const requestParams = {
+            responseType: 'text' as 'json',
+            headers: {'Content-Type': 'application/json'}
+        };
+        if (updatedBook.id == 0) {
+            return this.http.post<string>(this.apiUrl, updatedBook.toJsonString(),
+                requestParams
+            );
+        } else {
+            return this.http.put<string>(this.apiUrl, updatedBook.toJsonString(),
+                requestParams
+            );
+        }
+    }
+
+    deleteBook(bookId: number): Observable<string> {
+        return this.http.delete<string>(this.apiUrl + "/" + bookId,
+            {responseType: 'text' as 'json'}
+        );
+    }
+}
